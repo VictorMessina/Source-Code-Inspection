06612991f5d5c7fd67e1aa389a7a524451c7ccaf
anavitoriallerena <>
2016-03-27 14:40:30 -0300

    Classe PapelMoedaInvalidaException - Issues de Codigo #28

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -5,5 +5,8 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
-    
+
+    public PapelMoedaInvalidaException() {
+        super("Papel moeda inserido de forma incorreta, favor tentar novamente");
+    }
 }
